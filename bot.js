e00868845292cdde9097365c7a15eb7c8a07cc74
Ando <55718737+AndoNorth@users.noreply.github.com>
2023-02-25 18:38:20 +0000

    1) added slash commands 2) fix package.json 3) add first mp3 audio to play

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
+// setup .env
 require('dotenv').config()
 
-const {Client, Events, GatewayIntentBits} = require('discord.js')
+const {Client, Events, GatewayIntentBits, Collection} = require('discord.js')
+// setup bot intent
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -9,16 +11,54 @@ const client = new Client({
         GatewayIntentBits.GuildMembers,
     ],
     
-});
+})
+// setup commands
+const fs = require('node:fs')
+const path = require('node:path')
 
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands')
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles){
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if('data' in command && 'execute' in command){
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] the command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+// initialize the events
 client.once(Events.ClientReady, () =>{
-    console.log(`logged in as ${client.user.tag}!`)
+    console.log(`logged in as ${client.user.tag}!`);
 })
 
 client.on(Events.MessageCreate, msg => {
     if(msg.content === 'ping'){
-        msg.reply('pong')
+        msg.reply('pong');
     }
+})
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
 });
 
+// start bot
 client.login(process.env.BOT_TOKEN)
